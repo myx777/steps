3e99e071eba2b979c22f11f6af8203b3a682575f
anton <anton@Noutbuk-anton.local>
2024-01-23 15:54:29 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FormStep } from "./components/FormStep";
+
+function App() {
+
+  return (
+    <>
+    <FormStep />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FormStep.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import './Formstep.css';
+
+export const FormStep: FC = () => {
+
+
+    return (
+        <div>
+        <label htmlFor=""></label>
+            <form action="">
+            <input type="text" />
+            <input type="text" />
+            <button></button>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            </form>
+        </div>
+    )
+
+}
